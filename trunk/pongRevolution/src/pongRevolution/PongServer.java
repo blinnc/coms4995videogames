ed60e48eb1acf96ae0d31dafccf73396ecdf5bea
blinnc <blinnc@0f495da1-f1ad-ae5c-2d97-b5760cc2a85c>
2010-11-15 02:19:40 +0000

    failure code

--- trunk/pongRevolution/src/pongRevolution/PongServer.java
+++ trunk/pongRevolution/src/pongRevolution/PongServer.java
@@ -3,6 +3,8 @@ package pongRevolution;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.apache.thrift.TException;
+
 //Class that starts the server
 
 public class PongServer {
@@ -17,8 +19,18 @@ public class PongServer {
 		game = new Game();
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new ClockThread(), 0, GameSettings.CLOCK_INTERVAL);
-		new CommandServer(game);
-		new PollServer(game);
+		final CommandServer csrv = new CommandServer(game);
+		final PollServer psrv = new PollServer(game);
+		(new Thread() {
+            public void run() {
+                csrv.start();
+            }
+        }).start();
+		(new Thread() {
+            public void run() {
+            	psrv.start();
+            }
+        }).start();
 	}
 	
 	class ClockThread extends TimerTask {
